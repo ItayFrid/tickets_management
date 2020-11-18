b429bd7e4a975db29e567772b2b84c5b1b31f722
Itay Fridman <itayf@omc.co.il>
2020-11-18 15:41:00 +0200

    github integration

--- client/src/components/CategoryItem.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class CategoryItem extends Component {
-  render() {
-    const { category } = this.props;
-    return <div className="my-1">{category.name}</div>;
-  }
-}
